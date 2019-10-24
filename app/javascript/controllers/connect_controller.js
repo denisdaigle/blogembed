f693dc2f2e535e4e7de181547e233e4da5530df3
Denis Daigle <dadaigle@gmail.com>
2019-10-24 16:49:58 +0000

    stimulus JS setup

--- app/javascript/controllers/connect_controller.js
+++ app/javascript/controllers/connect_controller.js
@@ -1,10 +1,40 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-	static targets = ["thankyou", "form"]
+	static targets = ["thankyou", "form", "input", "error"]
 	
-	submit()	{
-		this.thankyouTarget.textContent = "Thank you! An email is on its way!"
-		this.formTarget.classList.add("hidden")
+	submit() {
+		if(this.inputTarget.value == ""){
+			//inform the user.
+			this.errorTarget.textContent = "Please provide an email address"
+			this.errorTarget.classList.remove("hidden")
+			//prevent form submit.
+			event.preventDefault()
+		} else if (this.validateEmail() == false) {
+			//inform the user.
+			this.errorTarget.textContent = "Please provide a valid email address"
+			this.errorTarget.classList.remove("hidden")
+			//prevent form submit.
+			event.preventDefault()
+		} else {
+			//allow form submit.
+			this.thankyouTarget.textContent = "thank you! An email is on its way!"
+			this.thankyouTarget.classList.remove("hidden")
+			this.formTarget.classList.add("hidden")
+		}
+
 	}
-}
+
+	validateEmail(){
+	
+		//Validate email format before sending
+		var email_validator = new RegExp('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
+		
+		if (email_validator.test(this.inputTarget.value)) {
+			return true
+		} else {
+			return false
+		}
+	
+	}
+} 
