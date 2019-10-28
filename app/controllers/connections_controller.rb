c9a0f1538455a4815f74090f33a72230a547e8f4
Denis Daigle <dadaigle@gmail.com>
2019-10-28 16:24:57 +0000

    tying in post-mailchimp welcome page stub

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -2,20 +2,6 @@ class ConnectionsController < ApplicationController
   
   def connect
     
-    #Email to me about new visitor.
-    @recipient_name = "New Visitor"
-    @recipient_email = params[:email] 
-    @subject = "Thanks for signing up for BlogEmbed!"
-    @body = "Hello, thanks for signing up for BlogEmbed. We're not quite ready for show time yet, but when we are, we'll let you know how to get started!"
-    GeneralMailer.general_email(@recipient_name, @recipient_email, @subject, @body).deliver
-
-    #Email to visitor
-    @recipient_name1 = "signup"
-    @recipient_email1 = "denis@blogembed.com"
-    @subject1 = "Hey Denis, you have a new guest"
-    @body1 = "Hey Denis, #{params[:email]} has asked to be part of this thing!"
-    GeneralMailer.general_email(@recipient_name1, @recipient_email1, @subject1, @body1).deliver
-
     #Add to MailChimp list.
     
     #Set access to key based on environment.
@@ -38,6 +24,13 @@ class ConnectionsController < ApplicationController
     ensure
       # always executed
     end
+    
+    #Email to visitor
+    @recipient_name1 = "new signup"
+    @recipient_email1 = "denis@blogembed.com"
+    @subject1 = "Hey Denis, you have a new guest"
+    @body1 = "Hey Denis, #{params[:email]} has asked to be part of this thing!"
+    GeneralMailer.general_email(@recipient_name1, @recipient_email1, @subject1, @body1).deliver
 
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/try'
   get 'pages/login'
+  
+  get '/welcome' => "pages#welcome"
+  
   root "pages#home" 
 end
