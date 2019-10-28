77a9d235633236197bc488d09fee14cf16ba7a5f
Denis Daigle <dadaigle@gmail.com>
2019-10-28 15:40:25 +0000

    adding mailchimp connection

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'mailchimp-api', '2.0.6', require: 'mailchimp'
--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -16,6 +16,29 @@ class ConnectionsController < ApplicationController
     @body1 = "Hey Denis, #{params[:email]} has asked to be part of this thing!"
     GeneralMailer.general_email(@recipient_name1, @recipient_email1, @subject1, @body1).deliver
 
+    #Add to MailChimp list.
+    
+    #Set access to key based on environment.
+    @mailchimp_key = ""
+    if Rails.env == "production"
+      @mailchimp_key = ENV['MAILCHIMP_KEY']
+    else
+      @mailchimp_key = Rails.application.secrets.mailchimp_key
+    end
+    
+    client = Mailchimp::API.new(@mailchimp_key)
+  
+    begin
+      sub_single_user = client.lists.subscribe('2e13e45505', {email: params[:email]})
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+    else
+      # other exception
+    ensure
+      # always executed
+    end
+
   end
   
 end
