c70307db1a1d88f16e46835aefd5b06745866d47
Denis Daigle <dadaigle@gmail.com>
2019-10-31 11:44:51 +0000

    adding initial html and style for profile setup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   post 'connections/connect'
   get '/welcome' => "pages#welcome"
+  
+  #USERS
   get '/confirm_account' => "users#confirm_account"
+  post '/user_profile_setup' => "users#profile_setup"
   get '/login' => "users#login"
   
   root "pages#home" 
