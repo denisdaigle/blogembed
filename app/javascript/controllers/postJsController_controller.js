0eab00b43c6423e60b492f35e6ea4ae41201cd80
Denis Daigle <dadaigle@gmail.com>
2019-12-02 22:54:19 +0000

    adding ability to set estimated display height to iframe

--- app/javascript/controllers/postJsController_controller.js
+++ app/javascript/controllers/postJsController_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-	static targets = ["code"]
+	static targets = ["code", "content", "iframeParent"]
 
 	toggle(event) {
 		
 		//hide the broadcast message
 		this.codeTarget.classList.toggle("hidden")
+		
+		var iframe_content = this.iframeParentTarget.value
+		var captured_height = this.contentTarget.clientHeight
+		this.iframeParentTarget.value = iframe_content.replace('estimated_display_dimensions', 'width:100%,height:' + captured_height + 'px')
 
 	}
 
